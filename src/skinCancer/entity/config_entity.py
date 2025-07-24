a7a3b7cca63d327a3eaeb9f8d39f16829ad7f4b1
Tushar <td220627@gmail.com>
2025-07-24 19:55:36 +0530

    Entity Added

---
+++ src/skinCancer/entity/config_entity.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+@dataclass(frozen=True)
+class DataIngestionConfig:
+    """
+    Configuration entity for the data ingestion component.
+    
+    Attributes:
+        root_dir (Path): The root directory where the data ingestion artifacts will be stored.
+        source_url (str): The URL of the website to scrape for images.
+        local_data_file (Path): The path to the local directory where scraped images will be saved.
+    """
+    root_dir: Path
+    source_url: str
+    local_data_file: Path
