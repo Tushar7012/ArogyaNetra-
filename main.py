61cbc73cc0a98fefd153c6b45af675f8e7411d71
Tushar <td220627@gmail.com>
2025-07-25 15:44:10 +0530

    Code Updated

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from skinCancer.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
+from skinCancer.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -10,6 +11,16 @@ try:
     
     print(f">>>>>> Stage: {STAGE_NAME} completed <<<<<<\n\nx==========x")
 
+except Exception as e:
+    print(f"An error occurred in {STAGE_NAME}: {e}")
+    raise e
+
+STAGE_NAME = "Data Validation stage"
+try:
+    print(f">>>>>> Stage: {STAGE_NAME} started <<<<<<")
+    data_validation_pipeline = DataValidationTrainingPipeline()
+    data_validation_pipeline.main()
+    print(f">>>>>> Stage: {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     print(f"An error occurred in {STAGE_NAME}: {e}")
     raise e
